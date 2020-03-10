be2febe63e9669c42e6b3ebd7fc57b0b5fee2b6d
tamas.kenessey@gmail.com <tamas.kenessey@gmail.com>
2020-03-10 09:48:00 +0100

    Add EIT logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Row, Col } from 'reactstrap';
 import { StyledLink2 } from '../StyledComponents/StyledComponents'; 
 import Met3rLogo from '../../images/logo-met3r-color.png';
+import EITlogo from '../../images/logo_EIT.png'
 import { Colors } from '../../modules/colors';
 
 const StyledFooterContainer = styled.div`
@@ -15,6 +16,7 @@ const StyledFooterContainer = styled.div`
 const StyledFooterLogo = styled.img`
     display: inline-block;
     max-width: 100px;
+    margin: 0 2rem;
     `;
 const StyledFooterLink = styled(StyledLink2)`
     font-size: 1.2rem;
@@ -31,6 +33,8 @@ const Footer = (props) => (
                     <Col xs={12} sm={6} className="text-left">
                         <StyledFooterLogo src={Met3rLogo} /> @All rights reserved, 2017-2019</Col>
                     <Col xs={12} sm={6} className="text-right">
+                        <span>Supported by: </span>
+                        <StyledFooterLogo src={EITlogo} alt="" />
                         <StyledFooterLink href="https://medium.com/met3r" >
                             <FontAwesomeIcon icon={['fab', 'medium']} size="lg" />
                         </StyledFooterLink>
